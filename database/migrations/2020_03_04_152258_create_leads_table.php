3e3b55501213a4269ad038a864bb56673a8b6a8a
WhozThiz <whozthiz@hotmail.com>
2020-03-04 12:36:00 -0300

    Initial Leads Table Commit

---
+++ database/migrations/2020_03_04_152258_create_leads_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('tax_id');
+            $table->string('registration_id');
+            $table->string('address');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->char('state');
+            $table->char('zipcode');
+            $table->char('homephone');
+            $table->char('mobile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leads');
+    }
+}
