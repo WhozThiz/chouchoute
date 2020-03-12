8e480233a82d0841eae77d3d8cf6e4469a6c96c1
WhozThiz <whozthiz@hotmail.com>
2020-03-12 09:57:45 -0300

    Disabled AutoComplete Username and Password

--- resources/js/views/login/index.vue
+++ resources/js/views/login/index.vue
@@ -63,8 +63,8 @@ export default {
     };
     return {
       loginForm: {
-        email: 'admin@laravue.dev',
-        password: 'laravue',
+        email: '',
+        password: '',
       },
       loginRules: {
         email: [{ required: true, trigger: 'blur', validator: validateEmail }],
