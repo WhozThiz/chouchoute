5c62dc383ae5cf1f50e0de0f94684909d27789f9
WhozThiz <whozthiz@hotmail.com>
2020-06-13 22:04:37 -0300

    Updated Articles Module

--- app/Http/Controllers/Cms/ArticleController.php
+++ app/Http/Controllers/Cms/ArticleController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\Cms;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Cms\ArticleImageController;
-use Illuminate\Http\Request;
-use App\Laravue\Models\Cms\Article;
 use App\Http\Resources\Cms\ArticleResource;
+use App\Laravue\Models\Cms\Article;
+use Illuminate\Http\Request;
 use App\Laravue\JsonResponse;
-use App\Laravue\Faker;
+use Validator;
 
 class ArticleController extends Controller
 {
@@ -21,48 +21,13 @@ class ArticleController extends Controller
     public function index()
     {
         // Get articles
-        $articles = Article::with('article_images')->orderBy('created_at', 'desc')->paginate(10);
+        $articles = Article::with('article_images')->orderBy('created_at', 'desc')->paginate(20);
+        $total = Article::count();
 
         // Return collection of articles as a resource
-        return ArticleResource::collection($articles);
-		
-    }
-
-	/**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function ArticleList()
-    {
-		$articles = Article::orderBy('created_at', 'asc')->paginate(20);
-		$total = Article::count();
-		
-		$data = [];
-		
-		foreach ($articles as $item) {
-			$row = [
-                'id' => $item->id,
-                'display_time' => Faker::randomDateTime()->format('Y-m-d H:i:s'),
-                'title' => $item->title,
-                'author' => Faker::randomString(mt_rand(5, 10)),
-                'comment_disabled' => Faker::randomBoolean(),
-                'content' => $item->content,
-                'content_short' => Faker::randomString(mt_rand(30, 50)),
-                'status' => Faker::randomInArray(['deleted', 'published', 'draft']),
-                'forecast' => mt_rand(100, 9999) / 100,
-                'image_uri' => 'https://via.placeholder.com/400x300',
-                'importance' => mt_rand(1, 3),
-                'pageviews' => mt_rand(10000, 999999),
-                'reviewer' => Faker::randomString(mt_rand(5, 10)),
-                'timestamp' => Faker::randomDateTime()->getTimestamp(),
-                'type' => Faker::randomInArray(['US', 'VI', 'JA']),
-            ];
-
-            $data[] = $row;
-		}
-
-        return response()->json(new JsonResponse(['items' => $data, 'total' => $total]));
+        // return ArticleResource::collection($articles);
+        return response()->json(new JsonResponse(['items' => $articles, 'total' => $total]));
+        // return ArticleResource::collection(Article::all());
 		
     }
 
@@ -74,6 +39,7 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
+        /*
         $article = $request->isMethod('put') ? Article::findOrFail($request->article_id) : new Article;
 
         $article->id = $request->input('article_id');
@@ -89,6 +55,35 @@ class ArticleController extends Controller
         if($article->save()) {
             return new ArticleResource($article);
         }
+        */
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'title' => ['required']
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 403);
+        } else {
+            $params = $request->all();
+            $article = Article::create([
+                'title' => $params['title'],
+                'user_id'=> '1',
+                'authors' => $params['authors'],
+                'keywords' => $params['keywords'],
+                'abstract' => $params['abstract'],
+                'introduction' => $params['introduction'],
+                'body' => $params['body'],
+                'references' => $params['references'],
+                'publish_time' => $params['publish_time'],
+                'publish_status' => $params['publish_status'],
+                'comment_status' => $params['comment_status'],
+            ]);
+        }
+
+            return new ArticleResource($article);
         
     }
 	
@@ -98,25 +93,41 @@ class ArticleController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function UpdateArticle(Request $request)
+    public function update(Request $request, Article $article)
     {
 
-        $article->id = $request->input('id');
-		$article->display_time = $request->input('display_time');
-        $article->title = $request->input('title');
-		$article->type = $request->input('type');
-		$article->author = $request->input('author');
-		$article->content_short = $request->input('content_short');
-        $article->content = $request->input('content');
-		$article->status = $request->input('status');
-		$article->importance = $request->input('importance');
-		$article->timestamp = $request->input('timestamp');
-		$article->image_uri = $request->input('image_uri');
-
-        if($article->save()) {
-            return new ArticleResource($article);
+        if ($article === null) {
+            return response()->json(['error' => 'Article not found'], 404);
         }
-        
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'title' => ['required']
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 403);
+        } else {
+            $params = $request->all();
+            $article->name = $params['name'];
+            $article->title = $params['title'];
+            $article->user_id = '1';
+            $article->authors = $params['authors'];
+            $article->keywords = $params['keywords'];
+            $article->abstract = $params['abstract'];
+            $article->introduction = $params['introduction'];
+            $article->body = $params['body'];
+            $article->references = $params['references'];
+            $article->publish_time = $params['publish_time'];
+            $article->publish_status = $params['publish_status'];
+            $article->comment_status = $params['comment_status'];
+            $article->save();
+        }
+
+        return new ArticleResource($article);
+
     }
 
     /**
@@ -133,21 +144,6 @@ class ArticleController extends Controller
         // Return single article as a resource
         return new ArticleResource($article);
     }
-	
-	/**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function ArticleShow($id)
-    {
-        // Get article
-        $article = Article::findOrFail($id);
-
-        return response()->json(new JsonResponse($article));
-		
-    }
 
     /**
      * Remove the specified resource from storage.
@@ -155,13 +151,15 @@ class ArticleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Article $article)
     {
-        // Get article
-        $article = Article::findOrFail($id);
+        try {
+            $article->delete();
+        } catch (\Exception $ex) {
+            response()->json(['error' => $ex->getMessage()], 403);
+        }
 
-        if($article->delete()) {
-            return new ArticleResource($article);
-        }    
+        return response()->json(null, 204);
+
     }
 }
