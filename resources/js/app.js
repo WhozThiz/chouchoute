2215767419f8a3b8072bf12f0db4de7bfe272f52
WhozThiz <whozthiz@hotmail.com>
2020-06-13 22:06:32 -0300

    Disabled Temporary FontAwesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,14 +7,14 @@ import router from '@/router';
 import i18n from './lang'; // Internationalization
 import '@/icons'; // icon
 import '@/permission'; // permission control
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import * as filters from './filters'; // global filters
 
-library.add(faUserSecret);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+// library.add(faUserSecret);
+// Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(ElementUI, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
