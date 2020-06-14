2b7e49a7e7d0fa9fd50cc3fc4d4f3b5eb9feb8d4
WhozThiz <whozthiz@hotmail.com>
2020-06-13 22:10:16 -0300

    Updated Articles Module

--- resources/js/views/cms/articles/components/List.vue
+++ resources/js/views/cms/articles/components/List.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-table v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%">
+    <el-table v-loading="loading" :data="list" border fit highlight-current-row style="width: 100%">
       <el-table-column align="center" label="ID" width="80">
         <template slot-scope="scope">
           <span>{{ scope.row.id }}</span>
@@ -9,7 +9,7 @@
 
       <el-table-column min-width="300px" label="Title">
         <template slot-scope="{row}">
-          <router-link :to="'/administrator/articles/edit/'+row.id" class="link-type">
+          <router-link :to="'/articles/edit/'+row.id" class="link-type">
             <span>{{ row.title }}</span>
           </router-link>
         </template>
@@ -17,58 +17,28 @@
 
       <el-table-column align="center" label="Actions" width="120">
         <template slot-scope="scope">
-          <router-link :to="'/administrator/articles/edit/'+scope.row.id">
-            <el-button type="primary" size="small" icon="el-icon-edit">
+          <router-link :to="'/articles/edit/'+scope.row.id">
+            <el-button type="primary" size="small" icon="el-icon-edit" @click="handleEditForm(scope.row.id, scope.row.name);">
               Edit
             </el-button>
           </router-link>
         </template>
       </el-table-column>
     </el-table>
-
-    <pagination :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
   </div>
 </template>
 
 <script>
-import Pagination from '@/components/Pagination'; // Secondary package based on el-pagination
-import Resource from '@/api/resource';
-const articleResource = new Resource('articles');
-
 export default {
   name: 'ArticleList',
-  components: { Pagination },
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'info',
-        deleted: 'danger',
-      };
-      return statusMap[status];
-    },
-  },
-  data() {
-    return {
-      list: null,
-      total: 0,
-      listLoading: true,
-      listQuery: {
-        page: 1,
-        limit: 20,
+  props: {
+    list: {
+      type: Array,
+      default: () => {
+        return {
+          data: [],
+        };
       },
-    };
-  },
-  created() {
-    this.getList();
-  },
-  methods: {
-    async getList() {
-      this.listLoading = true;
-      const { data } = await articleResource.list(this.listQuery);
-      this.list = data.items;
-      this.total = data.total;
-      this.listLoading = false;
     },
   },
 };
